118d349b3dc3ac354a0c439cc07c2e0133785e6c
Xiang Gu <c0710204@gmail.com>
2016-10-01 04:58:08 +0000

    tech updeate

--- sci/index.php
+++ sci/index.php
@@ -17,16 +17,37 @@
 		<div role="tabpanel">
 		  <!-- Nav tabs -->
 		  <ul class="nav nav-tabs" role="tablist">
-			<li role="presentation" class="active"><a href="#tree1" aria-controls="tree1" role="tab" data-toggle="tab">材料工程学</a></li>
-			<li role="presentation"><a href="#tree2" aria-controls="tree2" role="tab" data-toggle="tab">感应技术</a></li>
-			<li role="presentation"><a href="#tree3" aria-controls="tree3" role="tab" data-toggle="tab">生物及应用物理学</a></li>
+			<li role="presentation" class="active"><a href="#tree1" aria-controls="tree1" role="tab" data-toggle="tab">联邦-材料工程学</a></li>
+			<li role="presentation"><a href="#tree2" aria-controls="tree2" role="tab" data-toggle="tab">联邦-感应技术</a></li>
+			<li role="presentation"><a href="#tree3" aria-controls="tree3" role="tab" data-toggle="tab">联邦-生物及应用物理学</a></li>
+			<li role="presentation"><a href="#tree4" aria-controls="tree4" role="tab" data-toggle="tab">帝国-帝国技术科研所ITRI-T1</a></li>
+			<li role="presentation"><a href="#people" aria-controls="people" role="tab" data-toggle="tab">人物志</a></li>
 		  </ul>
 
 		  <!-- Tab panes -->
 		  <div class="tab-content">
-			<div role="tabpanel" class="tab-pane active" id="tree1"><img src="img/1.png"></div>
-			<div role="tabpanel" class="tab-pane" id="tree2"><img src="img/2.png"></div>
-			<div role="tabpanel" class="tab-pane" id="tree3"><img src="img/3.png"></div>
+			<div role="tabpanel" class="tab-pane active" id="tree1">
+
+				<img src="img/1.png">
+			</div>
+			<div role="tabpanel" class="tab-pane" id="tree2">
+				<img src="img/2.png">
+			</div>
+			<div role="tabpanel" class="tab-pane" id="tree3">
+				<img src="img/3.png">
+			</div>
+			<div role="tabpanel" class="tab-pane" id="tree4">
+
+				<img src="img/4.png">
+			</div>
+			<div role="tabpanel" class="tab-pane" id="people">
+				<h3>提坦斯南美秘密实验基地 - ??????(??????)</h3>
+				<p>首席:顾骧(故乡/guxiang)</p>
+				<pre style="white-space: pre-line;"><p>【终于安静了啊】，听着声音采集器里传出这样的声音后，齐藤一在自己的办公室里也点了点头。他关掉监视器，接着又把头转向阿兰多。巴基纳的再生计划的负责人---联邦军的一名技术长官，一名有着微胖的身形，乱蓬蓬的非洲卷发，此刻正用紧张的眼神看着他的压抑男性</p></pre>
+				<h3>帝国 - Imperial Tech & Research Institue(ITRI)</h3>
+				<p>首席:????(暮暮雪/暮雪輕寒)</p>
+				<pre style="white-space: pre-line;"><p>CQkJCTxoMz5JbXBlcmlhbCBUZWNoICYgUmVzZWFyY2ggSW5zdGl0dWUoSVRSSSk8L2gzPg0KCQkJCTxwPummluW4rTo/Pz8o5pqu5pqu6ZuqL+aarumbqui8leWvkik8L3A+</p></pre>
+			</div>
 		  </div>
 
 		</div>
