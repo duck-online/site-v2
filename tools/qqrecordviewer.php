4720dd9f100e3c16f7fd0bbeb6ebc866ca6b8e5d
Xiang Gu <c0710204@gmail.com>
2018-07-02 17:18:39 +0800

    2

--- tools/qqrecordviewer.php
+++ tools/qqrecordviewer.php
@@ -3,11 +3,15 @@
         path:<input type='text' name='path' placeholder="linux path"><br>
     </form>
 </div>
+<pre>
 <?php
 //load file
 if ($_GET['path']){
     $txt=file_get_contents($_GET['path']);
+    $txt=preg_replace($txt,"\\d4-\\d2-\\d2 \\d2:\\d2:\\d2 某科学的可达鸭【gundam】\\(\\d*\\)","|duck|$0|");
     print($txt);
+    
 }
 
 ?>
+</pre>
